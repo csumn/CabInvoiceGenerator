3415acbf185e42ce77704e3328fe27ea86808e24
csumn <csumn13@gmail.com>
2022-04-17 11:48:00 +0530

    For multiple rides

--- CabInvoiceGenerator/src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ CabInvoiceGenerator/src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -9,4 +9,12 @@ public class InvoiceGenerator {
 	public double calculateFare(double distance, int time) {
 		return Math.max(MINIMUM_FARE, distance * COST_PER_KM + time * COST_PER_TIME);
 	}
+	
+	public double calculateFare(Ride[] rides) {
+		double totalFare = 0;
+		for (Ride ride : rides) {
+			totalFare += calculateFare(ride.getDistance(), ride.getTime());
+		}
+		return totalFare;
+	}
 }
